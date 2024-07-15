df9ba6582f9b44f50464dae4ec06801f033f325e
Dimitar <dimitarangelov25@gmail.com>
2024-07-15 10:25:15 +0300

    allowed hosts issue

--- mysite/settings.py
+++ mysite/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = env('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DJANGO_DEBUG')
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1','https://simple-web-production-7255.up.railway.app']
+ALLOWED_HOSTS = ['localhost','127.0.0.1','simple-web-production-7255.up.railway.app']
 CSRF_TRUSTED_ORIGINS = ['https://simple-web-production-7255.up.railway.app']
 
 
